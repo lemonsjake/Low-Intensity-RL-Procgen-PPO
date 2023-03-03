588d5897f54a0a260b22e2e6fecec2d9058abf3c
Lemons <89406861+lemonsjake@users.noreply.github.com>
2023-03-03 16:14:33 -0700

    Update ProcgenProject.py
    Procgen and PPO working successfully together

--- ProcgenProject.py
+++ ProcgenProject.py
@@ -2,16 +2,55 @@
 #import numpy as np  #
 #import seaborn as sns  #
 
+
+
+import gym
+from stable_baselines3 import PPO
+
+env = gym.make("procgen:procgen-coinrun-v0", render_mode="rgb_array")#"human"
+
+model = PPO("CnnPolicy", env, verbose=1)  #MlpPolicy --> CnnPolicy (because input is image)
+model.learn(total_timesteps=10_000)
+print("DONE LEARNING")
+#step = 0
+#step_limit = 1_000  #
+vec_env = model.get_env()
+obs = vec_env.reset()
+for i in range(1000):
+    action, _states = model.predict(obs, deterministic=True)
+    obs, reward, done, info = vec_env.step(action)
+    vec_env.render()
+
+
+
+'''
 import gym3
 from procgen import ProcgenGym3Env
 from stable_baselines3 import PPO
+
+env = ProcgenGym3Env(num=2, env_name="coinrun")#, render_mode="rgb_array")
+env = gym3.ViewerWrapper(env, info_key="rgb")
+#model = PPO("CnnPolicy", env, verbose=1)  #MlpPolicy --> CnnPolicy (because input is image)
+#model.learn(total_timesteps=10_000)
+#print("DONE LEARNING")
+step = 0
+step_limit = 1_000  #
+while step <= step_limit:  #True
+    sample_act = gym3.types_np.sample(env.ac_space, bshape=(env.num,))  #
+    env.act(sample_act)  #gym3.types_np.sample(env.ac_space, bshape=(env.num,))
+    #print(f"\nACTION: {sample_act}")  #
+    rew, obs, first = env.observe()
+    #print(f"OBSERVATION: {obs}")  #
+    print(f"step {step} reward {rew} first {first}")
+    step += 1
+
+
 
 env = ProcgenGym3Env(num=2, env_name="coinrun")#, render_mode="rgb_array")
 #env = gym3.ViewerWrapper(env, info_key="rgb")
 model = PPO("CnnPolicy", env, verbose=1)  #MlpPolicy --> CnnPolicy (because input is image)
 model.learn(total_timesteps=10_000)
 print("DONE LEARNING")
-#print(env.get_combos())  #
 #step = 0
 #step_limit = 1_000  #
 vec_env = model.get_env()
@@ -24,8 +63,28 @@ for i in range(1000):
     # if done:
     #   obs = env.reset()
 
-'''
+
+
+env = ProcgenGym3Env(num=2, env_name="coinrun")#, render_mode="rgb_array")
+env = gym3.ViewerWrapper(env, info_key="rgb")
+#model = PPO("CnnPolicy", env, verbose=1)  #MlpPolicy --> CnnPolicy (because input is image)
+#model.learn(total_timesteps=10_000)
+#print("DONE LEARNING")
+step = 0
+step_limit = 1_000  #
+while step <= step_limit:  #True
+    sample_act = gym3.types_np.sample(env.ac_space, bshape=(env.num,))  #
+    env.act(sample_act)  #gym3.types_np.sample(env.ac_space, bshape=(env.num,))
+    #print(f"\nACTION: {sample_act}")  #
+    rew, obs, first = env.observe()
+    #print(f"OBSERVATION: {obs}")  #
+    print(f"step {step} reward {rew} first {first}")
+    step += 1
+
+
+
 Look for stable-baselines3 for procgen implementation
+
 
 
 while step <= step_limit:  #True
@@ -36,6 +95,7 @@ while step <= step_limit:  #True
     #print(f"OBSERVATION: {obs}")  #
     print(f"step {step} reward {rew} first {first}")
     step += 1
+
 
 
 import gym
@@ -58,10 +118,6 @@ for i in range(1000):
 
 env.close()
 
-
-import githubimport
-from openai.procgen import env, interactive
-interactive.main()
 
 
 git status
