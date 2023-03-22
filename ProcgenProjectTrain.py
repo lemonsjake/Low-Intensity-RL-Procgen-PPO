c18db76eb3698b5a5ec1d6346594d4db524633c2
Joe Vincent <joevincentkc@gmail.com>
2023-03-22 10:28:42 -0700

    joe test the train/test files
    everything works for me

--- ProcgenProjectTrain.py
+++ ProcgenProjectTrain.py
@@ -2,15 +2,18 @@ import gym
 from stable_baselines3 import PPO
 from datetime import datetime
 
+timestamp = datetime.now().strftime('%d-%m-%y-%H_%M')
+
 print("\nBegin Learning")
 env = gym.make("procgen:procgen-coinrun-v0", num_levels=1,
                render_mode="rgb_array")
+
 model = PPO("CnnPolicy", env, verbose=1, 
             tensorboard_log="./tensorboard_logs/")
 
 model.learn(total_timesteps=25_000)
 print("\nLearning Complete")
 
-model.save(datetime.now().strftime('%d-%m-%y-%H_%M_'))
-del model  # delete trained model to demonstrate loading
+model.save("models/" + timestamp)
+
 print("\nSaved Model")
