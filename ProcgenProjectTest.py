efcef3e52eb1533dcbccc17aad3e8c5d9c29c8e8
Lemons <xlemonsjake@gmail.com>
2023-03-29 14:21:14 -0600

    Jake - test out saving/loading

--- ProcgenProjectTest.py
+++ ProcgenProjectTest.py
@@ -2,13 +2,13 @@ import gym
 from stable_baselines3 import PPO
 
 env_test = gym.make("procgen:procgen-coinrun-v0", num_levels=1, render_mode="human")
-model = PPO.load("models/22-03-23-10_20", env=env_test)
+model = PPO.load("models/29-03-23-14_12", env=env_test)
 print("\nLoaded Model")
 
 print("\nTesting Model")
 vec_env = model.get_env()
 obs = vec_env.reset()
-for i in range(500):
+for i in range(250):
     if (i % 10) == 0:
         print("step = " + str(i))
     action, _states = model.predict(obs, deterministic=True)
--- ProcgenProjectTrain.py
+++ ProcgenProjectTrain.py
@@ -11,9 +11,11 @@ env = gym.make("procgen:procgen-coinrun-v0", num_levels=1,
 model = PPO("CnnPolicy", env, verbose=1, 
             tensorboard_log="./tensorboard_logs/")
 
-model.learn(total_timesteps=25_000)
+model.learn(total_timesteps=1_000)
 print("\nLearning Complete")
 
 model.save("models/" + timestamp)
 
 print("\nSaved Model")
+
+### change num_levels (line 8), how does profficiency change? or a similar question.
