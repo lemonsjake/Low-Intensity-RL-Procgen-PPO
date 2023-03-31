68be9d528b36bb96dde5c276f94532685f7b23a7
Lemons <xlemonsjake@gmail.com>
2023-03-31 14:51:23 -0600

    Jake - Trained 15 agents
    Each agent differs by num_levels, total_timesteps, or both.

--- ProcgenProjectTrain.py
+++ ProcgenProjectTrain.py
@@ -5,13 +5,13 @@ from datetime import datetime
 timestamp = datetime.now().strftime('%d-%m-%y-%H_%M')
 
 print("\nBegin Learning")
-env = gym.make("procgen:procgen-coinrun-v0", num_levels=1,
-               render_mode="rgb_array")
+env = gym.make("procgen:procgen-coinrun-v0", num_levels=100,
+               render_mode="rgb_array")  # num_levels = 1, 10, 25, 50, 100
 
 model = PPO("CnnPolicy", env, verbose=1, 
             tensorboard_log="./tensorboard_logs/")
 
-model.learn(total_timesteps=1_000)
+model.learn(total_timesteps=25_000)  # total_timesteps = 5_000, 25_000, 50_000, 125_000
 print("\nLearning Complete")
 
 model.save("models/" + timestamp)
