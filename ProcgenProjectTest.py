f4b5a71374dbf41e82f69b5e5712e0cbc816092f
Lemons <xlemonsjake@gmail.com>
2023-04-13 00:55:29 -0600

    Trained/Tested 10 versions of nl=0

--- ProcgenProjectTest.py
+++ ProcgenProjectTest.py
@@ -1,14 +1,15 @@
 import gym
 from stable_baselines3 import PPO
 
-env_test = gym.make("procgen:procgen-coinrun-v0", num_levels=1, render_mode="human")  # num_levels = 1, 10, 25, 50, 100
-model = PPO.load("models/04-04-23-14_29", env=env_test)
+
+env_test = gym.make("procgen:procgen-coinrun-v0", num_levels=0, render_mode="human")  # num_levels = 1, 10, 25, 50, 100
+model = PPO.load("models/experimental/nl0_10__13-04-23-00_05", env=env_test)
 print("\nLoaded Model")
 
 print("\nTesting Model")
 vec_env = model.get_env()
 obs = vec_env.reset()
-for i in range(300):  #250, 500, etc
+for i in range(1000):  #250, 500, etc
     if (i % 100) == 0:   #10, 100, etc
         print("step = " + str(i))
     action, _states = model.predict(obs, deterministic=True)
--- ProcgenProjectTrain.py
+++ ProcgenProjectTrain.py
@@ -4,49 +4,22 @@ from stable_baselines3 import PPO
 from datetime import datetime
 
 #timestamp = datetime.now().strftime('%d-%m-%y-%H_%M')
+for i in range(1, 11):
+    print("i: ", i)
 
-for n_levels in [1,3,5]:
-    print("n_levels: ", n_levels)
-
-    if n_levels == 1:
-        for i in range(3, 11):
-            print("i: ", i)
-
-            timestamp = datetime.now().strftime('%d-%m-%y-%H_%M')
-            print(timestamp)
+    timestamp = datetime.now().strftime('%d-%m-%y-%H_%M')
+    print(timestamp)
             
-            print("\nBegin Learning")
-            env = gym.make("procgen:procgen-coinrun-v0", num_levels=n_levels,
-                        render_mode="rgb_array")  # num_levels = 1, 3, 5
+    print("\nBegin Learning")
+    env = gym.make("procgen:procgen-coinrun-v0", num_levels=0,
+                render_mode="rgb_array")  # num_levels = 1, 3, 5
 
-            model = PPO("CnnPolicy", env, verbose=1, 
-                        tensorboard_log="./tensorboard_logs/multi_policy_experiment/")
+    model = PPO("CnnPolicy", env, verbose=1, 
+                tensorboard_log="./tensorboard_logs/multi_policy_experiment/")
 
-            model.learn(total_timesteps=25_000)
-            print("\nLearning Complete")
+    model.learn(total_timesteps=25_000)
+    print("\nLearning Complete")
 
-            model.save("models/experimental/" + "nl" + str(n_levels) + "_" + str(i) + "__" + timestamp)
-            #model.save("models/" + timestamp)
-            print("\nSaved Model")
-    else:
-        for i in range(1, 11):
-            print("i: ", i)
-
-            timestamp = datetime.now().strftime('%d-%m-%y-%H_%M')
-            print(timestamp)
-            
-            print("\nBegin Learning")
-            env = gym.make("procgen:procgen-coinrun-v0", num_levels=n_levels,
-                        render_mode="rgb_array")  # num_levels = 1, 3, 5
-
-            model = PPO("CnnPolicy", env, verbose=1, 
-                        tensorboard_log="./tensorboard_logs/multi_policy_experiment/")
-
-            model.learn(total_timesteps=25_000)
-            print("\nLearning Complete")
-
-            model.save("models/experimental/" + "nl" + str(n_levels) + "_" + str(i) + "__" + timestamp)
-            #model.save("models/" + timestamp)
-            print("\nSaved Model")
-
-### change num_levels (line 8), how does profficiency change? or a similar question.
+    model.save("models/experimental/" + "nl" + str(0) + "_" + str(i) + "__" + timestamp)
+    #model.save("models/" + timestamp)
+    print("\nSaved Model")
